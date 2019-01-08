3fde3867a419f2984b33e62b4c9d05f9b23d018c
microsong <jhn06046@hotmail.com>
2019-01-08 23:29:59 +0900

    실전 1-6

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-alert("i'm working. my name is JS. I'm beautiful. I'm worth it")
+//Variable(변수) = 변할 수 있는 것
+//라인 = expression 한 expression에 여러 개를 적는 방법은 ;를 적어서 구분해준다.
+//하지만 한 줄에 여러 가지 변수 선언은 좋지 않음
+
+//변수의 Create -> 변수의 Initialize -> 변수의 Use
+
+//let은 변수의 선언
+let a = 221;
+b = a - 5;
+a = 4;  //변수의 업데이트
+console.log(b, a);
